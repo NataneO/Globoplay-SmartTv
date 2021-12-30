63275ea6a42077aa0c581fff4762d8b6f78d341a
NataneO <nataneoliveira9@gmail.com>
2021-12-30 13:50:21 -0300

    Adicionando botoes e features

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # Globoplay - SmartTV
 Projeto teste para vaga Globoplay - SmartTv
+
+____________________________________________________________________________
+                        *Melhorias da feature*
+
+- Adicionado modal de player
+- Adicionada mensagem de "Adicionado à sua lista""
+- Adicionado modal de lista de conteudo
+- Adicionado botão curtir com efeito
+
+- Enter (13) adicionado às teclas de navegação
+____________________________________________________________________________
+*- Como instalar o projeto?*
+
+
+Efetuar o clone através do seu repositório e executar o comando 'npm install'
+
+
+
+
+*- Como navegar pelo projeto?*
+
+
+Após rodar o projeto, acessar o link http://localhost:8080/
+A navegação é feita através das teclas direcionais com base nas premissas do desafio.
+
+
+*- Como buildar o projeto?*
+
+
+Executar o comando 'npm build'
+
+# Observações acerca do desafio 
+
+Adorei executar o desafio proposto pela Globo! Me trouxe diversos aprendizados e percepções e trabalhar construindo toda uma estrutura apenas com Canvas foi realmente desafiador rs :)
+
+*- Foi utilizada alguma biblioteca para o desenvolvimento da navegação?*
+
+Nanani-na-não! Toda a navegação foi desenvolvida por mim.
+
+*- E para o desenvolvimento da interface?*
+
+Também não. O único recurso externo utilizado foi o pacote FontAwesome.
+
+*- Mentira! Mas não foi utilizada NENHUMA lib de gerenciamento de estado? Ou libs desnecessárias?*
+
+0 libs, amigos. o/
+
+*- Mas houve algum problema durante o projeto?*
+
+Em partes. Praticamente todos os requisitos foram cumpridos, e alguns com extras rs.
+Por exemplo: a responsividade funciona ( então os tamanhos de tela que foram pedidos, estão sendo compreendidos), porém, a cada alteração no tamanho da tela, é necessário *atualizar* a página para que dê tudo certinho.
+
+*- E a arquitetura? Porquê criar dessa forma?*
+
+Bem, eu tinha uma semana para um projeto de teste, onde minhas habilidades estavam à prova. Escolhi o mais simples possível (Html, CSS e JsVanilla) para me dedicar ao real objetivo do desafio. Depois, tudo isso foi empacotado com webpack :)
+
+
+Pretendo continuar implementando novas funcionalidades em branchs desse mesmo projeto!
+
+# Existirão melhorias?
+
+Sim. Nas outras branchs desse projeto existirão novas features, para fins de estudo ( caso tenha interesse, contribua!)
+
+abração <3
--- src/main.js
+++ src/main.js
@@ -16,15 +16,9 @@ let showDetails = false;
 window.addEventListener('keydown', KeyDown, true);
 window.addEventListener('resize', resizeCanvas, false);
 
-let ageRating = new Map();
-ageRating.set('L', 'assets/img/L.png');
-ageRating.set('10', 'assets/img/10.png');
-ageRating.set('12', 'assets/img/12.png');
-ageRating.set('14', 'assets/img/14.png');
-ageRating.set('16', 'assets/img/16.png');
-ageRating.set('18', 'assets/img/18.png');
 
-let redraw=false
+
+let redraw = "0"
 initData(redraw);
 
 function initData(redraw) {
@@ -37,33 +31,61 @@ function initData(redraw) {
       let img = new Image();
       img.src = data.images.cover;
       img.onload = function () {
-        primaryItens(img,data,redraw)
-        if (redraw == true){
-          detailsBody(showDetails,redraw)
-          detailsContent(data, redraw)
-        } else {
-          secondaryItems(data)
+        switch (redraw) {
+          case "1":
+            primaryItens(img, data, redraw)
+            detailsBody(showDetails, redraw)
+            detailsContent(data, redraw)
+            break;
+
+          case "2":
+            primaryItens(img, data, redraw)
+            boxContent(focus);
+            break;
+
+          case "3":
+            primaryItens(img, data, redraw)
+            secondaryItems(data);
+            listMessage(data);
+            break;
+
+          case "4":
+            primaryItens(img, data, redraw)
+            secondaryItems(data)
+          break;
+
+          case "0":
+            primaryItens(img, data, redraw)
+            secondaryItems(data)
+            break;
+
         }
+        console.log(redraw)
       };
       return "ok"
     })
-    
+
 }
 
 function primaryItens(img, data, redraw) {
-  domContext.drawImage(img, 80, 0, myWidth, myHeight);
+  if (redraw == "0" || redraw=="2" || redraw=="4") {
+    domContext.drawImage(img, 80, 0, myWidth, myHeight);
+  } else {
+
+  }
+
   gradient = domContext.createLinearGradient(myWidth, myHeight, 0, 100);
   for (t = 1; t >= 0; t -= 0.02) {
     gradient.addColorStop(t, "hsla(360, 100%, 0%, " + easeInOut(t) * 1 + ")");
   }
-  if (redraw==false){
+  if (redraw == "0" || redraw=="2" || redraw=="4") {
     domContext.fillStyle = gradient;
     spacingLeft = 80
   } else {
     domContext.fillStyle = "rgba(0, 0, 0, 0.9)";
     spacingLeft = 0
   }
- 
+
 
   domContext.fillRect(spacingLeft, 0, myWidth, myHeight);
 
@@ -75,7 +97,7 @@ function secondaryItems(data) {
   drawDescription(data.description)
   drawClassification(data.classification, redraw);
   dataBody(data.category, data.genres, data.year)
-  detailsBody(showDetails,redraw);
+  detailsBody(showDetails, redraw);
   detailsContent(data, redraw)
   drawButtons(redraw, focus);
 }
@@ -84,41 +106,66 @@ function KeyDown(evt) {
   let before = 0
   switch (evt.keyCode) {
     case 38:
-     
+
       break;
     case 40:
-      redraw = true;
+      redraw = "1";
       showDetails = true;
       before = focus
       focus = 1
       domContext.clearRect(0, 0, domContext.canvas.width, domContext.canvas.height)
       initData(redraw)
-      
+
       break;
     case 37:
-      
+
       focus = focus - 1
       if (focus < 0) {
         focus = 0;
       }
-      
+
       drawButtons(redraw, focus)
-   
+
       break;
     case 39:
-      if (redraw==true){
+      if (redraw == "1") {
         showDetails = false;
-        redraw = false;
+        redraw = "0";
         detailsBody(showDetails, redraw)
-      } else{
-      focus = focus + 1;
-      if (focus > 3) {
-        focus = 3;
+      } else {
+        focus = focus + 1;
+        if (focus > 4) {
+          focus = 4;
+        }
       }
-    }
 
       drawButtons(redraw, focus)
       break;
+
+    case 13:
+      switch (focus) {
+        case 0:
+          console.log(focus + "Voltar")
+
+          break;
+        case 1:
+          drawBox();
+          break;
+
+        case 2:
+          drawAddList()
+          break;
+
+        case 3:
+          drawBox();
+          break;
+
+        case 4:
+          redraw = "4"
+          initData(redraw)
+          break;
+
+      }
 
   }
 
@@ -140,22 +187,22 @@ function drawTitle(title) {
 function drawDescription(description) {
   let spacingLeft = 0
   let spacingTop = 0
-  if (redraw==true){
+  if (redraw == "1") {
     maxWidth = domContext.canvas.width / 3
-    spacingLeft =  domContext.canvas.width/2
+    spacingLeft = domContext.canvas.width / 2
     spacingTop = 300
-    
+
   } else {
     alertExclusiveContent()
     maxWidth = domContext.canvas.width / 2
-    spacingLeft = domContext.canvas.width/15
+    spacingLeft = domContext.canvas.width / 15
     spacingTop = domContext.canvas.height / 3.2
   }
- 
+
 
 
   domContext.font = "1em Open Sans,sans-serif"
-  
+
 
 
   lineHeight = 25;
@@ -190,22 +237,29 @@ function wrapText(domContext, description, x, y, maxWidth, lineHeight) {
   domContext.fillText(line, x, y);
 }
 
-function drawClassification(classification,redraw) {
+function drawClassification(classification, redraw) {
+  let ageRating = new Map();
+  ageRating.set('L', 'assets/img/L.png');
+  ageRating.set('10', 'assets/img/10.png');
+  ageRating.set('12', 'assets/img/12.png');
+  ageRating.set('14', 'assets/img/14.png');
+  ageRating.set('16', 'assets/img/16.png');
+  ageRating.set('18', 'assets/img/18.png');
   let ageRatingIcon = new Image();
   ageRatingIcon.src = ageRating.get(classification);
 
-  if (redraw == false){
-    spacingTop = domContext.canvas.height/8
+  if (redraw == "0") {
+    spacingTop = domContext.canvas.height / 8
     spacingLeft = domContext.canvas.width / 15
-    } else {
-      spacingTop=domContext.canvas.height/3.55
-      spacingLeft = domContext.canvas.width / 11.5
-    }
-    ageRatingIcon.onload = function () {
-      domContext.drawImage(ageRatingIcon, spacingLeft , spacingTop, 20, 20);
-  
+  } else {
+    spacingTop = domContext.canvas.height / 3.55
+    spacingLeft = domContext.canvas.width / 11.5
   }
+  ageRatingIcon.onload = function () {
+    domContext.drawImage(ageRatingIcon, spacingLeft, spacingTop, 20, 20);
+
   }
+}
 
 
 
@@ -217,14 +271,14 @@ function dataBody(category, genres, year) {
 
 }
 
-function detailsBody(showDetails,redraw) {
+function detailsBody(showDetails, redraw) {
   marginTop = 0;
   detailsHeight = 0
   if (showDetails == false) {
-    if (redraw==true){
-      domContext.clearRect(0,0,domContext.canvas.width, domContext.canvas.height)
-    } 
-    
+    if (redraw == "1") {
+      domContext.clearRect(0, 0, domContext.canvas.width, domContext.canvas.height)
+    }
+
     initData(redraw);
     marginTop = halfHeight + 200;
     detailsHeight = 200;
@@ -239,51 +293,53 @@ function detailsBody(showDetails,redraw) {
   width = document.documentElement.clientWidth
 
   domContext.fillStyle = "#222";
- 
+
   larg = 200000
   top = 90
   domContext.fillRect(domContext.canvas.width / 15, marginTop + 30, width - ((width / 15) * 2), detailsHeight)
   domContext.fillStyle = "#fff";
-  
+
 
 }
 
-function detailsContent(data,redraw){
-    if(redraw==false){
-        domContext.fillStyle='#333'
-    } else{
-        domContext.fillStyle='#fff'
-    }
-  domContext.fillText("Titulo original: "+data.title, domContext.canvas.width / 10, marginTop + 80)
+function detailsContent(data, redraw) {
+  if (redraw == "0") {
+    domContext.fillStyle = '#333'
+  } else {
+    domContext.fillStyle = '#fff'
+  }
+  domContext.fillText("Titulo original: " + data.title, domContext.canvas.width / 10, marginTop + 80)
   domContext.fillText("Nao ha inadequacoes ", domContext.canvas.width / 9, marginTop + 110)
-  drawClassification(data.classification,redraw)
-  domContext.fillText("Genero: "+ data.genres[1], domContext.canvas.width / 2, marginTop + 80)
+  drawClassification(data.classification, redraw)
+  domContext.fillText("Genero: " + data.genres[1], domContext.canvas.width / 2, marginTop + 80)
   domContext.fillText("Pais: ", domContext.canvas.width / 2, marginTop + 110)
-  domContext.fillText("Ano de lancamento: "+data.year, domContext.canvas.width / 2, marginTop + 140)
+  domContext.fillText("Ano de lancamento: " + data.year, domContext.canvas.width / 2, marginTop + 140)
   domContext.fillText("Sinopse: ", domContext.canvas.width / 2, marginTop + 170)
   drawDescription(data.description)
-  
+
 }
 
 
 function drawButtons(redraw, focus) {
   btnValue = new Map();
-  if (redraw == true){
+  
+  if (redraw == "1") {
     btnValue.set('Voltar', '\uF060');
     focus = 0;
   } else {
- 
-  btnValue.set('Voltar', '\uF060');
-  btnValue.set('Assista', '\uF04b');
-  btnValue.set('Minha lista', '\uF055');
-  btnValue.set('Capitulos', '\uF26c');
-  marginRight = domContext.canvas.width / 15
-  domContext.clearRect(domContext.canvas.height / 30, domContext.canvas.height / 2.4, 345, 100)
-  domContext.clearRect(marginRight, domContext.canvas.height / 2.4, 345, 100)
+    let iconVariation = (redraw=="4"?'\uF004':'\uF184');
+    btnValue.set('Voltar', '\uF060');
+    btnValue.set('Assista', '\uF04b');
+    btnValue.set('Minha lista', '\uF055');
+    btnValue.set('Capitulos', '\uF26c');
+    btnValue.set('Curtir', iconVariation);
+    marginRight = domContext.canvas.width / 15
+    domContext.clearRect(domContext.canvas.height / 30, domContext.canvas.height / 2.4, (120 * btnValue.length), 100)
+    domContext.clearRect(marginRight, domContext.canvas.height / 2.4, 345, 100)
   }
-  
+
   domContext.strokeStyle = "white";
-  
+
   i = 0
   active = 0
 
@@ -331,9 +387,45 @@ function drawIcons(active, marginRight, name, icon) {
     domContext.font = '.8em Open Sans, sans-serif'
     domContext.fillText(name, (marginRight + ((100 - domContext.measureText(name).width) / 2)), domContext.canvas.height / 1.9);
   }
- 
+
   domContext.measureText(name).width
 
 }
 
+function drawBox() {
+  console.log(redraw)
+  domContext.clearRect(0, 0, domContext.canvas.width, domContext.canvas.height)
+  redraw = "2";
+  initData(redraw)
+}
+
+function boxContent(focus) {
+  let content = "";
+  if (focus == "1") {
+    content = "Esse espaco fica reservado para o player"
+  } else if (focus == "3") {
+    content = "Esse espaco fica reservado para a listagem de capítulos"
+  }
+
+  domContext.fillStyle = "#333";
+  domContext.fillRect(domContext.canvas.width / 15, domContext.canvas.height / 8, domContext.canvas.width - ((domContext.canvas.width / 15) * 2), domContext.canvas.height - ((domContext.canvas.height / 8) * 2))
+  domContext.fillStyle = "#fff";
+  domContext.fillText(content, (marginRight + ((100 - domContext.measureText(content).width) / 2)), domContext.canvas.height / 1.9)
+}
+
+
+function drawAddList() {
+  console.log(redraw)
+  domContext.clearRect(0, 0, domContext.canvas.width, domContext.canvas.height)
+  redraw = "3";
+  initData(redraw)
+}
+
+function listMessage(data) {
+  domContext.fillStyle = "#333";
+  domContext.fillRect(domContext.canvas.width / 15, domContext.canvas.height / 8, domContext.canvas.width - ((domContext.canvas.width / 15) * 2), domContext.canvas.height - ((domContext.canvas.height / 8) * 7))
+  domContext.fillStyle = "#fff";
+  let content = "A " + (data.category).toLowerCase() + " '" + data.title + "' foi adicionada a sua lista :)"
+  domContext.fillText(content, (marginRight + ((100 - domContext.measureText(content).width) / 2)), domContext.canvas.height - ((domContext.canvas.height / 8) * 6.5))
+}
 
